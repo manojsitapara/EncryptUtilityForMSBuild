5bb20ca6d2c51647088f0746cccee61cca0320fb
Manoj Sitapara <manojsitapara@gmail.com>
2018-03-07 17:51:10 +0530

    Add project files.

---
+++ EncryptUtilityForMSBuild/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace EncryptUtilityForMSBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"input.txt");
+
+            Class1 c = new Class1();
+            string encryptString = c.Encrypt(text);
+
+            //Write to a file
+            using (StreamWriter writer = new StreamWriter(@"output.txt"))
+            {
+                writer.WriteLine(encryptString);
+            }
+
+            
+
+
+        }
+    }
+}
